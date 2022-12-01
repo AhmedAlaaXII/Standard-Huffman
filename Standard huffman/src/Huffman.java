dba187f15340da435e0c3e3d2346e3d54226be07
ahmed alaa <afh7766@gmail.com>
2022-12-01 14:42:26 +0200

    the code of Standard Huffman using java

---
+++ Standard huffman/src/Huffman.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Huffman {
+
+    public static <string> void main(String[] args)  {
+        GUI gui=new GUI();
+
+
+    }
+}
